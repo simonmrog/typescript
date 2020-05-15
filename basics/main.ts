40cd0d33452fd2dc6280858928364204e94402b4
simonmrog <simonmrog@gmail.com>
2020-05-14 23:24:17 -0500

    initial commit

---
+++ basics/main.ts
@@ -0,0 +1,16 @@
+class Customer {
+  name: string;
+
+  constructor(name: string) {
+    this.name = name;
+  }
+
+  announce() {
+    return "Hello, my name is " + this.name;
+  }
+}
+
+let firstCustomer = new Customer("Alice");
+let newMessage: string = firstCustomer.announce();
+let webHeading = document.querySelector("h1");
+webHeading!.textContent = newMessage;
