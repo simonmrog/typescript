c8d3ca0dc8a4813b6c6412248e2fdbf045719864
simonmrog <simonmrog@gmail.com>
2021-04-02 12:45:14 -0500

    practicing typescript

---
+++ typescript-fast/main.js
@@ -0,0 +1,5 @@
+var n = 3;
+var message = "Hello World";
+console.log(message + ": " + n);
+var variable;
+console.log(variable);
---
+++ typescript-fast/main.ts
@@ -0,0 +1,7 @@
+const n: number = 3;
+const message: string  = "Hello World";
+
+console.log(`${message}: ${n}`);
+
+let variable: number;
+console.log(variable)
