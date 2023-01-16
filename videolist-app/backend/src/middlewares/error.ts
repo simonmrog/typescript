440569c73882afff3cd6ddde5f63c8ae90ad9af9
Simón Murillo Gómez <simonmrog@Simons-MacBook-Air.local>
2023-01-16 18:51:41 -0500

    feature: error middleware

---
+++ videolist-app/backend/src/middlewares/error.ts
@@ -0,0 +1,18 @@
+import { Request, Response, NextFunction } from "express";
+
+export const errorHandler = (err: any, req: Request, res: Response, next: NextFunction): void => {
+  let statusCode = 500;
+  console.error("[ERROR MIDDLEWARE]", err);
+
+  if (err.message.toLowerCase().includes("cast to objectid failed for value")) {
+    statusCode = 400;
+    err.message = "Invalid ID"
+  }
+
+  if (err.message.toLowerCase().includes("not found"))
+    statusCode = 404;
+  res.status(statusCode).json({
+    status: "error",
+    message: err.message
+  });
+}
