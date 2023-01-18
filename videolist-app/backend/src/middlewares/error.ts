3347b7f8feb041ea4a3a63be0ff18f68cc339cdc
Simón Murillo Gómez <simonmrog@Simons-MacBook-Air.local>
2023-01-18 14:41:32 -0500

    updating 400 status code for error

--- videolist-app/backend/src/middlewares/error.ts
+++ videolist-app/backend/src/middlewares/error.ts
@@ -4,6 +4,9 @@ export const errorHandler = (err: any, req: Request, res: Response, next: NextFu
   let statusCode = 500;
   console.error("[ERROR MIDDLEWARE]", err);
 
+  if (err.message.includes("The URL already exists in DB"))
+    statusCode = 400;
+
   if (err.message.toLowerCase().includes("cast to objectid failed for value")) {
     statusCode = 400;
     err.message = "Invalid ID"
