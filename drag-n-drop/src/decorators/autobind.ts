ba769c8d7e3ea41915cab67123123361f830e547
Simón Murillo Gómez <simonmrog@Simons-MacBook-Air.local>
2023-03-13 18:29:40 -0500

    refactoring file structure

---
+++ drag-n-drop/src/decorators/autobind.ts
@@ -0,0 +1,17 @@
+namespace App {
+  export function Autobind(
+    _target: any,
+    _methodName: string,
+    descriptor: PropertyDescriptor
+  ) {
+    const originalMethod = descriptor.value;
+    const adjDescriptor: PropertyDescriptor = {
+      configurable: true,
+      get() {
+        const boundFn = originalMethod.bind(this);
+        return boundFn;
+      },
+    };
+    return adjDescriptor;
+  }
+}
